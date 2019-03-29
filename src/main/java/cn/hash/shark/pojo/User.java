f94c0d0500ecf71a0d4c06068fc5812fe537c25b
shark <wuqi0502@qq.com>
2019-03-29 16:11:23 +0800

    lombok和blog页面

--- src/main/java/cn/hash/shark/pojo/User.java
+++ src/main/java/cn/hash/shark/pojo/User.java
@@ -1,8 +1,12 @@
 package cn.hash.shark.pojo;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 public class User {
 
     private long id;
