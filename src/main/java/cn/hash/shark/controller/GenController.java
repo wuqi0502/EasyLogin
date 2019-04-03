242d8bd83e177c639b6c02c5f220487ef5ad38d6
shark <wuqi0502@qq.com>
2019-04-03 17:04:13 +0800

    Restful

--- src/main/java/cn/hash/shark/controller/GenController.java
+++ src/main/java/cn/hash/shark/controller/GenController.java
@@ -2,11 +2,14 @@ package cn.hash.shark.controller;
 
 import cn.hash.shark.service.GenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+/**
+ * 通用控制层
+ */
+@RestController
 public class GenController {
 
     @Autowired
